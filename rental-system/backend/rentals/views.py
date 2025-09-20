cc5b1194a38cb0d224da88902a3b78e1b6fd7574
Boniface Masila <dietboni@gmail.com>
2025-09-20 11:00:50 +0300

    add views for login

--- rental-system/backend/rentals/views.py
+++ rental-system/backend/rentals/views.py
@@ -1,9 +1,10 @@
 from django.http import HttpResponse
+from django.contrib.auth import authenticate
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework import status
-from .serializers import SignupSerializer
+from .serializers import SignupSerializer, LoginSerializer
 
 def home(request):
     return HttpResponse("<h1>Welcome to the Rental System Backend!</h1>")
@@ -18,4 +19,17 @@ class SignupView(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response({"message": "User created successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class LoginView(APIView):
+    def post(self, request):
+        serializer = LoginSerializer(data=request.data)
+        if serializer.is_valid():
+            user = authenticate(
+                username=serializer.validated_data['username'],
+                password=serializer.validated_data['password']
+            )
+            if user:
+                return Response({"message": "Login successful."}, status=status.HTTP_200_OK)
+            return Response({"error": "Invalid credentials."}, status=status.HTTP_401_UNAUTHORIZED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
