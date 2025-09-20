39a9264603070420667ec58fed33bfae8de09158
Boniface Masila <dietboni@gmail.com>
2025-09-20 11:15:35 +0300

    urls for the dashboards

--- rental-system/backend/rental_system/urls.py
+++ rental-system/backend/rental_system/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("", views.home, name="home"),  # Django homepage
     path('api/signup/', SignupView.as_view(), name='signup'),
     path('api/login/', LoginView.as_view(), name='login'),
+    path("api/tenant-dashboard/", views.tenant_dashboard, name="tenant-dashboard"),
+    path("api/landlord-dashboard/", views.landlord_dashboard, name="landlord-dashboard"),
 ]
