0ea93044ad80d72dca1025a1de372ce177eb4db3
Boniface Masila <dietboni@gmail.com>
2025-09-20 10:41:33 +0300

    Add SignupSerializer for user registration with password confirmation validation

--- rental-system/backend/rentals/serializers.py
+++ rental-system/backend/rentals/serializers.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from .models import Rental
 
@@ -5,3 +6,21 @@ class RentalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Rental
         fields = '__all__'
+
+class SignupSerializer(serializers.ModelSerializer):
+    confirmPassword = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password', 'confirmPassword']
+        extra_kwargs = {'password': {'write_only': True}}
+
+    def validate(self, data):
+        if data['password'] != data['confirmPassword']:
+            raise serializers.ValidationError("Passwords do not match.")
+        return data
+
+    def create(self, validated_data):
+        validated_data.pop('confirmPassword')
+        user = User.objects.create_user(**validated_data)
+        return user
